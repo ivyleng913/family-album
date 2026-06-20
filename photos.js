49f10c9c71055b1ef6762e09b73c906658e8bf28
Lengyingrui <ivy.leng913@gmail.com>
2026-06-20 23:11:31 +0800

    2026/6/20 v2.0

--- photos.js
+++ photos.js
@@ -1,30 +1,40 @@
 const photos = [
   {
     image: "images/photo9.png",
+    layout: "portrait-single",
     date: "1998?",
-    location: "我不知道",
-    text: "非常般配又帅又美！"
+    location: "我不知道在哪里",
+    text: "非常般配，又帅又美！"
   },
   {
-    image: ["images/photo29.png", "images/photo30.png", "images/photo31.png", "images/photo41.png"],
+    image: [
+      "images/photo29.png",
+      "images/photo30.png",
+      "images/photo31.png",
+      "images/photo41.png"
+    ],
+    layout: "quad",
     date: "2023/8/16",
     location: "英国剑桥",
     text: "第一次出国有很多回忆，英国非常爱下雨，但我去的时候都是天晴"
   },
   {
     image: "images/photo32.png",
+    layout: "wide-single",
     date: "2023/11/22",
     location: "新疆库尔勒",
     text: "辽阔的大漠衬得人很帅"
   },
   {
     image: "images/photo33.png",
+    layout: "portrait-single",
     date: "2023/12/5",
     location: "疑似在绵阳",
-    text: "妈妈的同学聚会~"
+    text: "妈妈的同学聚会～"
   },
   {
     image: "images/photo28.png",
+    layout: "portrait-single",
     date: "2023/12/17",
     location: "不知道在哪",
     text: "像丰收的喜悦哈哈哈"
@@ -37,45 +47,59 @@ const photos = [
   },
   {
     image: "images/photo13.png",
+    layout: "portrait-single",
     date: "2025/5/14",
     location: "感觉在小区里",
     text: "老爸陪奶奶散步"
   },
   {
     image: "images/photo11.png",
+    layout: "portrait-single",
     date: "2025/5/20",
     location: "家里！",
     text: "收到花花的老妈很开心"
   },
   {
     image: "images/photo7.png",
+    layout: "portrait-single",
     date: "2025/6/15",
     location: "在家！",
-    text: "去年父亲节给老爸买了花~ 时间过得好快，整整一年了"
+    text: "去年父亲节给老爸买了花～时间过得好快，整整一年了"
   },
   {
     image: "images/photo12.png",
+    layout: "wide-single",
     date: "2025/6/21",
     location: "四川北川",
     text: "风景大不如前"
   },
   {
-    image: ["images/photo10.png", "images/photo20.png"],
+    image: [
+      "images/photo10.png",
+      "images/photo20.png"
+    ],
+    layout: "panorama-pair",
     date: "2025/7/13",
     location: "四川九鼎山",
     text: "这个地方我印象太深，美得像户外广告"
   },
   {
-    image: ["images/photo4.png", "images/photo19.png", "images/photo21.png", "images/photo22.png"],
+    image: [
+      "images/photo4.png",
+      "images/photo19.png",
+      "images/photo21.png",
+      "images/photo22.png"
+    ],
+    layout: "quad",
     date: "2025/8/7",
     location: "四川雅安蒙顶山",
-    text: "景色太好了 看起来玩的很开心"
+    text: "景色太好了，看起来玩得很开心"
   },
   {
     image: "images/photo1.png",
     date: "2025/8/17",
     location: "四川剑阁",
-    text: "热的不行找了个小馆子，好像味道还可以"
+    text: "热得不行找了个小馆子，好像味道还可以"
   },
   {
     image: "images/photo14.png",
@@ -84,19 +108,32 @@ const photos = [
     text: "这是我实验室哈哈哈，在重新装修"
   },
   {
-    image: ["images/photo3.png", "images/photo5.png", "images/photo16.png"],
+    image: [
+      "images/photo3.png",
+      "images/photo5.png",
+      "images/photo16.png"
+    ],
+    layout: "two-top-one-bottom",
     date: "2026/2/16",
     location: "四川绵阳",
     text: "回家过年！我本命年哈哈哈"
   },
   {
-    image: ["images/photo8.png", "images/photo18.png"],
+    image: [
+      "images/photo8.png",
+      "images/photo18.png"
+    ],
+    layout: "pair",
     date: "2026/2/23",
     location: "四川自贡",
-    text: "看完发现欣赏不来灯会 但挺开心！"
+    text: "看完发现欣赏不来灯会，但挺开心！"
   },
   {
-    image: ["images/photo15.png", "images/photo40.png"],
+    image: [
+      "images/photo15.png",
+      "images/photo40.png"
+    ],
+    layout: "pair",
     date: "2026/2/24",
     location: "四川乐山",
     text: "乐山确实好吃，下次还吃"
@@ -108,7 +145,11 @@ const photos = [
     text: "这天天气特别好"
   },
   {
-    image: ["images/photo24.png", "images/photo25.png"],
+    image: [
+      "images/photo24.png",
+      "images/photo25.png"
+    ],
+    layout: "panorama-pair",
     date: "2026/5/2",
     location: "虎牙",
     text: "五月还有这么大的雪"
@@ -127,9 +168,10 @@ const photos = [
   },
   {
     image: "images/photo38.png",
+    layout: "portrait-single",
     date: "这个真的不知道",
     location: "",
-    text: "我小时候~"
+    text: "我小时候～"
   }
 ];
 
@@ -137,21 +179,63 @@ const photos = [
   const gallery = document.getElementById("gallery");
   if (!gallery) return;
 
+  const columnLayouts = {
+    quad: [[0, 1], [2, 3]],
+    "mountain-mix": [[0, 1], [2, 3]]
+  };
+
+  function renderImage(image, alt) {
+    return `
+      <figure class="photo-frame">
+        <img src="${image}" alt="${alt}" loading="lazy" />
+      </figure>`;
+  }
+
+  function renderImages(images, layout, alt) {
+    const columns = columnLayouts[layout];
+
+    if (columns) {
+      return columns
+        .map(
+          (column) => `
+        <div class="photo-column">
+          ${column.map((index) => renderImage(images[index], alt)).join("")}
+        </div>`
+        )
+        .join("");
+    }
+
+    return images.map((image) => renderImage(image, alt)).join("");
+  }
+
   gallery.innerHTML = photos
-    .map(
-      (photo, i) => `
-    <div class="photo-card${i % 2 === 1 ? " reverse" : ""}">
+    .map((photo, i) => {
+      const images = Array.isArray(photo.image) ? photo.image : [photo.image];
+      const isMulti = images.length > 1;
+      const layout = photo.layout || (isMulti ? "group" : "single");
+
+      const cardClasses = [
+        "photo-card",
+        isMulti ? "multi" : "single",
+        `layout-${layout}`,
+        `count-${images.length}`,
+        i % 2 === 1 ? "reverse" : ""
+      ]
+        .filter(Boolean)
+        .join(" ");
+
+      return `
+    <div class="${cardClasses}">
       <div class="photo-images">
-        ${(Array.isArray(photo.image) ? photo.image : [photo.image])
-          .map((image) => `<img src="${image}" alt="${photo.location || '照片'}" />`)
-          .join("")}
+        ${renderImages(images, layout, photo.location || "照片")}
       </div>
+
       <div class="photo-text">
-        ${photo.date ? `<p class="date">${photo.date}${photo.location ? ` · ${photo.location}` : ""}</p>` : ""}
+        ${photo.date ? `<p class="date">${photo.date}</p>` : ""}
         ${photo.location ? `<h3>${photo.location}</h3>` : ""}
         ${photo.text ? `<p>${photo.text}</p>` : ""}
       </div>
-    </div>`
-    )
+    </div>`;
+    })
     .join("");
 })();
