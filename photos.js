f4fde42e4019a0162deeb8528ee2d1ae1d9f9239
Lengyingrui <ivy.leng913@gmail.com>
2026-06-20 22:13:00 +0800

    2026/6/20

--- photos.js
+++ photos.js
@@ -1,21 +1,135 @@
 const photos = [
   {
-    image: "images/photo1.png",
-    date: "小时候",
-    location: "上海",
-    text: "小时候总觉得你的手很大，只要被你牵着，就什么都不用害怕。"
+    image: "images/photo9.png",
+    date: "1998?",
+    location: "我不知道",
+    text: "非常般配又帅又美！"
+  },
+  {
+    image: ["images/photo29.png", "images/photo30.png", "images/photo31.png", "images/photo41.png"],
+    date: "2023/8/16",
+    location: "英国剑桥",
+    text: "第一次出国有很多回忆，英国非常爱下雨，但我去的时候都是天晴"
+  },
+  {
+    image: "images/photo32.png",
+    date: "2023/11/22",
+    location: "新疆库尔勒",
+    text: "辽阔的大漠衬得人很帅"
+  },
+  {
+    image: "images/photo33.png",
+    date: "2023/12/5",
+    location: "疑似在绵阳",
+    text: "妈妈的同学聚会~"
+  },
+  {
+    image: "images/photo28.png",
+    date: "2023/12/17",
+    location: "不知道在哪",
+    text: "像丰收的喜悦哈哈哈"
   },
   {
     image: "images/photo2.png",
-    date: "某一年夏天",
-    location: "杭州西湖",
-    text: "很多当时觉得普通的瞬间，后来回头看，才发现都是最珍贵的回忆。"
+    date: "2025/3/7",
+    location: "上海",
+    text: "还在物理学院的时候，在主持学术活动"
   },
   {
-    image: "images/photo3.png",
-    date: "现在",
-    location: "青岛海边",
-    text: "以前是你陪我长大，以后也希望我能多陪你，把日子过得慢一点。"
+    image: "images/photo13.png",
+    date: "2025/5/14",
+    location: "感觉在小区里",
+    text: "老爸陪奶奶散步"
+  },
+  {
+    image: "images/photo11.png",
+    date: "2025/5/20",
+    location: "家里！",
+    text: "收到花花的老妈很开心"
+  },
+  {
+    image: "images/photo7.png",
+    date: "2025/6/15",
+    location: "在家！",
+    text: "去年父亲节给老爸买了花~ 时间过得好快，整整一年了"
+  },
+  {
+    image: "images/photo12.png",
+    date: "2025/6/21",
+    location: "四川北川",
+    text: "风景大不如前"
+  },
+  {
+    image: ["images/photo10.png", "images/photo20.png"],
+    date: "2025/7/13",
+    location: "四川九鼎山",
+    text: "这个地方我印象太深，美得像户外广告"
+  },
+  {
+    image: ["images/photo4.png", "images/photo19.png", "images/photo21.png", "images/photo22.png"],
+    date: "2025/8/7",
+    location: "四川雅安蒙顶山",
+    text: "景色太好了 看起来玩的很开心"
+  },
+  {
+    image: "images/photo1.png",
+    date: "2025/8/17",
+    location: "四川剑阁",
+    text: "热的不行找了个小馆子，好像味道还可以"
+  },
+  {
+    image: "images/photo14.png",
+    date: "2025/9/26",
+    location: "上海交通大学电院5号楼103",
+    text: "这是我实验室哈哈哈，在重新装修"
+  },
+  {
+    image: ["images/photo3.png", "images/photo5.png", "images/photo16.png"],
+    date: "2026/2/16",
+    location: "四川绵阳",
+    text: "回家过年！我本命年哈哈哈"
+  },
+  {
+    image: ["images/photo8.png", "images/photo18.png"],
+    date: "2026/2/23",
+    location: "四川自贡",
+    text: "看完发现欣赏不来灯会 但挺开心！"
+  },
+  {
+    image: ["images/photo15.png", "images/photo40.png"],
+    date: "2026/2/24",
+    location: "四川乐山",
+    text: "乐山确实好吃，下次还吃"
+  },
+  {
+    image: "images/photo23.png",
+    date: "2026/4/18",
+    location: "上海交通大学主图书馆附近",
+    text: "这天天气特别好"
+  },
+  {
+    image: ["images/photo24.png", "images/photo25.png"],
+    date: "2026/5/2",
+    location: "虎牙",
+    text: "五月还有这么大的雪"
+  },
+  {
+    image: "images/photo26.png",
+    date: "2026/5/3",
+    location: "江苏南京玄武湖",
+    text: "太适合跑步了，上海就没有这么舒服的地方"
+  },
+  {
+    image: "images/photo27.png",
+    date: "2026/6/14",
+    location: "湖南长沙橘子洲头",
+    text: "想起了自己小时候"
+  },
+  {
+    image: "images/photo38.png",
+    date: "这个真的不知道",
+    location: "",
+    text: "我小时候~"
   }
 ];
 
@@ -27,11 +141,15 @@ const photos = [
     .map(
       (photo, i) => `
     <div class="photo-card${i % 2 === 1 ? " reverse" : ""}">
-      <img src="${photo.image}" alt="${photo.location}" />
+      <div class="photo-images">
+        ${(Array.isArray(photo.image) ? photo.image : [photo.image])
+          .map((image) => `<img src="${image}" alt="${photo.location || '照片'}" />`)
+          .join("")}
+      </div>
       <div class="photo-text">
-        <p class="date">${photo.date} · ${photo.location}</p>
-        <h3>${photo.location}</h3>
-        <p>${photo.text}</p>
+        ${photo.date ? `<p class="date">${photo.date}${photo.location ? ` · ${photo.location}` : ""}</p>` : ""}
+        ${photo.location ? `<h3>${photo.location}</h3>` : ""}
+        ${photo.text ? `<p>${photo.text}</p>` : ""}
       </div>
     </div>`
     )
